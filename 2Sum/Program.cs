e8034fbcf55c139353fc3f363896c72a3b92fc6b
batsoyla <vasilikitsiapakidou@gmail.com>
2020-12-10 17:17:33 +0100

    Add files via upload

---
+++ 2Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _2Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<String> num = new List<String>();
+            using (StreamReader sr = new StreamReader(@"C:\Users\Vasiliki\Desktop\coursera algorithms\algorithms coursera\week4\input.txt"))
+            {
+                string line;
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    num.Add(line);
+                }
+            }
+            Console.WriteLine(num.Count);
+            HashSet<long> result = new HashSet<long>();
+            twoSum Sum = new twoSum();
+            result = Sum.TwoSum(num, -10000, 10000);
+            Console.WriteLine(result.Count);
+
+        }
+    }
+}
---
+++ 2Sum/twoSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2Sum
+{
+    class twoSum
+    {
+        public HashSet<long> TwoSum(List<String> numbers, long Target1, long Target2)
+        {
+            HashSet<long> values = new HashSet<long>();
+            HashSet<long> result = new HashSet<long>();
+            foreach (String num in numbers)
+            {
+                long value = Int64.Parse(num);
+                for(long i=Target1; i<= Target2; i++)
+                {
+                    long target = i - value ;
+                    if(values.Contains(target))
+                    {
+                        result.Add(i);
+                    }
+                    values.Add(value);
+
+                }
+
+            }
+            return result;
+        }
+    }
+}
